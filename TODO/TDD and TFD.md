d17d80115c9807b2b2c67b36445f3b9c831e1897
Horsed <legendaryhorsed@gmail.com>
2014-05-22 21:11:55 +0200

    added deploy script

---
+++ TODO/TDD and TFD.md
@@ -0,0 +1,10 @@
+---
+title: TDD and TFD
+author: martin
+date: 2014-
+template: article.jade
+descr: When I write tests and when I write them first
+---
+  # learning
+  # new terrain: frameworks, libs, domain
+  # dynamically typed languages
--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 cd ./src &&
 wintersmith build &&
 cd .. &&
-rm -rf articles &&
-rm -rf css &&
-rm -rf page &&
-rm -rf todo &&
-rm archive.html &&
-rm feed.xml &&
-rm index.html &&
-cp -r src/build/* ./
+rm -rf articles
+rm -rf css
+rm -rf page
+rm archive.html
+rm feed.xml
+rm index.html
+cp -r src/build/* ./ &&
+git add . && git add . -u && git commit && git push origin master
